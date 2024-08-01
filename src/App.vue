a3fe2fe83ea8d2b4e14aee7b1c350327f4d194f4
gaaabrielsantos <grvs.93@gmail.com>
2024-08-01 07:35:17 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,19 @@
-<script setup>
-  import { reactive } from 'vue';
-  import Cabecalho from './components/Cabecalho.vue';
-  import Formulario from './components/Formulario.vue';
-  import ListaDeTarefas from './components/ListaDeTarefas.vue';
+<script>
+import Calculadora from './components/Calculador.vue';
 
-const estado = reactive({
-  tarefaTemporaria: '',
-  filtro: 'todas',
-  tarefas: [
-    {
-      titulo: 'Estudar Vue.js',
-      finalizada: false,
-    },
-    {
-      titulo: 'Estudar SCSS',
-      finalizada: false,
-    },
-    {
-      titulo: 'Ir ao cinema',
-      finalizada: true,
-    }
-  ]
-});
-
-const getTarefasPendentes = () => {
-  return estado.tarefas.filter(tarefa => !tarefa.finalizada);
-};
-
-const getTarefasFinalizadas = () => {
-  return estado.tarefas.filter(tarefa => tarefa.finalizada);
-};
-
-const getTarefasFiltradas = () => {
-  const { filtro } = estado;
-
-  switch (filtro) {
-    case 'pendentes':
-      return getTarefasPendentes();
-    case 'finalizadas':
-      return getTarefasFinalizadas();
-    default:
-      return estado.tarefas;
-  };
-};
-
-const cadastraTarefa = () => {
-  const tarefaNova = {
-    titulo: estado.tarefaTemporaria,
-    finalizada: false,
-  };
-
-  estado.tarefas.push(tarefaNova);
-  estado.tarefaTemporaria = '';
+export default {
+  name: 'App',
+  components: {
+    Calculadora,
+  },
 };
 </script>
 
 <template>
-  <div class="container">
-    <Cabecalho :tarefas-pendentes="getTarefasPendentes().length"/>
-    <Formulario :trocar-filtro="evento => estado.filtro = evento.target.value" :tarefa-temporaria="estado.tarefaTemporaria" :edita-tarefa-temporaria="evento => estado.tarefaTemporaria = evento.target.value" :cadastra-tarefa="cadastraTarefa"/>
-    <ListaDeTarefas :tarefas="getTarefasFiltradas()"/>
-  </div>
+<div id="app">
+  <Calculadora />
+</div>
 </template>
+
+<style>
+</style>
